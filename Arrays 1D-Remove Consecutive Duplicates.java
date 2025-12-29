6971b53c7a5d571e6f4d51a4077e39a28c348c51
Saravana Krishnan J <168951123+SaravanaKrishnan16@users.noreply.github.com>
2025-12-29 19:09:19 +0530

    Stack

---
+++ Arrays 1D-Remove Consecutive Duplicates.java
@@ -0,0 +1,51 @@
+You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them. We repeatedly make duplicate removals on s until we can no longer. Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+
+Input Format
+
+Input consists of a string.
+
+Constraints
+
+NA
+
+Output Format
+
+Print the string after removing consecutive duplicates.
+
+Sample Input 0
+
+abbaca
+
+Sample Output 0
+
+ca
+
+Explanation 0
+
+For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move. The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+import java.io.*;
+import java.util.*;
+
+public class Solution{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        Stack<Character> st = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(!st.isEmpty() && st.peek()==ch){
+                st.pop();
+            } 
+            else{
+                st.push(ch);
+            }
+        }
+        StringBuilder res = new StringBuilder();
+        for (char ch : st){
+            res.append(ch);
+        }
+        System.out.print(res.toString());
+    }
+}
