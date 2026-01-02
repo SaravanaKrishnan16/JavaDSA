15b6e5ed85be5ef1635c2a9bb89a8c662249d25b
Saravana Krishnan J <168951123+SaravanaKrishnan16@users.noreply.github.com>
2026-01-02 18:50:43 +0530

    Arrays

---
+++ Sort 0s, 1s and 2s.java
@@ -0,0 +1,44 @@
+Given an array arr[] containing only 0s, 1s, and 2s. Sort the array in ascending order.
+
+Note: You need to solve this problem without utilizing the built-in sort function.
+
+Examples:
+
+Input: arr[] = [0, 1, 2, 0, 1, 2]
+Output: [0, 0, 1, 1, 2, 2]
+Explanation: 0s, 1s and 2s are segregated into ascending order.
+
+Input: arr[] = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1]
+Output: [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2]
+Explanation: 0s, 1s and 2s are segregated into ascending order.
+
+Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+
+Constraints:
+1 ≤ arr.size() ≤ 106
+0 ≤ arr[i] ≤ 2
+
+--------------------------------------------------------------------------------------------------------
+
+
+class Solution {
+    public void sort012(int[] arr) {
+        int low = 0, mid = 0, high = arr.length - 1;
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                swap(arr,low++,mid++);
+            } 
+            else if (arr[mid] == 1) {
+                mid++;
+            } 
+            else {
+                swap(arr,mid,high--);
+            }
+        }
+    }
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
